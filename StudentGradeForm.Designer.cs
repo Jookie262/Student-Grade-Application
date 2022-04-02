675316c629d69a313d586f1bcba3de8754993e7d
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-04-02 20:03:36 +0800

    Rename Components in Second Form Panel

--- StudentGradeForm.Designer.cs
+++ StudentGradeForm.Designer.cs
@@ -34,17 +34,17 @@ namespace StudentGradeApplication
             this.welcome_screen = new System.Windows.Forms.PictureBox();
             this.next_button1 = new System.Windows.Forms.PictureBox();
             this.second_panel = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.back_button1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.name_textbox = new Guna.UI2.WinForms.Guna2TextBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.next_button2 = new System.Windows.Forms.PictureBox();
             this.first_panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.welcome_screen)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.next_button1)).BeginInit();
             this.second_panel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back_button1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.next_button2)).BeginInit();
             this.SuspendLayout();
             // 
             // first_panel
@@ -82,26 +82,26 @@ namespace StudentGradeApplication
             // second_panel
             // 
             this.second_panel.BackgroundImage = global::StudentGradeApplication.Properties.Resources.bg1;
-            this.second_panel.Controls.Add(this.pictureBox3);
+            this.second_panel.Controls.Add(this.next_button2);
             this.second_panel.Controls.Add(this.name_textbox);
             this.second_panel.Controls.Add(this.pictureBox2);
-            this.second_panel.Controls.Add(this.pictureBox1);
+            this.second_panel.Controls.Add(this.back_button1);
             this.second_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.second_panel.Location = new System.Drawing.Point(0, 0);
             this.second_panel.Name = "second_panel";
             this.second_panel.Size = new System.Drawing.Size(334, 561);
             this.second_panel.TabIndex = 2;
             // 
-            // pictureBox1
+            // back_button1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::StudentGradeApplication.Properties.Resources.back;
-            this.pictureBox1.Location = new System.Drawing.Point(32, 96);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(75, 54);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.back_button1.BackColor = System.Drawing.Color.Transparent;
+            this.back_button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.back_button1.Image = global::StudentGradeApplication.Properties.Resources.back;
+            this.back_button1.Location = new System.Drawing.Point(32, 96);
+            this.back_button1.Name = "back_button1";
+            this.back_button1.Size = new System.Drawing.Size(75, 54);
+            this.back_button1.TabIndex = 0;
+            this.back_button1.TabStop = false;
             // 
             // pictureBox2
             // 
@@ -142,16 +142,16 @@ namespace StudentGradeApplication
             this.name_textbox.TabIndex = 2;
             this.name_textbox.TextOffset = new System.Drawing.Point(15, 0);
             // 
-            // pictureBox3
+            // next_button2
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox3.Image = global::StudentGradeApplication.Properties.Resources.NEXT;
-            this.pictureBox3.Location = new System.Drawing.Point(21, 328);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(184, 87);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.next_button2.BackColor = System.Drawing.Color.Transparent;
+            this.next_button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.next_button2.Image = global::StudentGradeApplication.Properties.Resources.NEXT;
+            this.next_button2.Location = new System.Drawing.Point(21, 328);
+            this.next_button2.Name = "next_button2";
+            this.next_button2.Size = new System.Drawing.Size(184, 87);
+            this.next_button2.TabIndex = 3;
+            this.next_button2.TabStop = false;
             // 
             // StudentGradeForm
             // 
@@ -167,9 +167,9 @@ namespace StudentGradeApplication
             ((System.ComponentModel.ISupportInitialize)(this.welcome_screen)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.next_button1)).EndInit();
             this.second_panel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back_button1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.next_button2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -181,8 +181,8 @@ namespace StudentGradeApplication
         private System.Windows.Forms.PictureBox next_button1;
         private System.Windows.Forms.Panel second_panel;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox back_button1;
+        private System.Windows.Forms.PictureBox next_button2;
         private Guna.UI2.WinForms.Guna2TextBox name_textbox;
     }
 }
